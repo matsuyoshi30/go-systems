c03b722f6edd1220657e6888c979effbc8607d0a
matsuyoshi <sfbgwm30@gmail.com>
2020-04-16 21:47:04 +0900

    add chapter07

---
+++ ch07/multicast/server/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"time"
+)
+
+const interval = 10 * time.Second
+
+func main() {
+	fmt.Println("Start tick server at 224.0.0.1:9999")
+
+	conn, err := net.Dial("udp", "224.0.0.1:9999")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	start := time.Now()
+	wait := start.Truncate(interval).Add(interval).Sub(start) // 現在時刻が XX:45 以降だった場合の考慮あり
+	time.Sleep(wait)
+	ticker := time.Tick(interval)
+
+	for now := range ticker {
+		conn.Write([]byte(now.String()))
+		fmt.Println("Tick: ", now.String())
+	}
+}
